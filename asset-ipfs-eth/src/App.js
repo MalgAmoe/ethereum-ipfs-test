048a6ecb3fd0a94aac2fe245fd3d91509713aac7
Yann Jubelin <yann.jubelin@utopiamusic.com>
2018-06-26 17:46:52 +0200

    upload image to ipfs

--- asset-ipfs-eth/src/App.js
+++ asset-ipfs-eth/src/App.js
@@ -26,12 +26,38 @@ class App extends Component {
     })
   }
 
+  addAsset = (event) => {
+    event.stopPropagation()
+    event.preventDefault()
+    const file = event.target.files[0]
+    let reader = new window.FileReader()
+    reader.readAsArrayBuffer(file)
+    reader.onloadend = () => this.saveToIpfs(reader)
+  }
+
+  saveToIpfs = (reader) => {
+    const buffer = Buffer.from(reader.result)
+    this.ipfs.add(buffer)
+      .then((response) => {
+        console.log(response)
+        this.setState({
+          added_file_hash: response[0].hash
+        })
+      }).catch((err) => {
+        console.error(err)
+      })
+  }
+
   render() {
     return (
       <div className="App">
         <p>Your ID is <strong>{this.state.id}</strong></p>
         <p>Your IPFS version is <strong>{this.state.version}</strong></p>
         <p>Your IPFS protocol version is <strong>{this.state.protocol_version}</strong></p>
+        <form id="captureMedia">
+          <input type="file" onChange={this.addAsset} />
+        </form>
+        {this.state.added_file_hash && <img src={`https://ipfs.io/ipfs/${this.state.added_file_hash}`}/>}
       </div>
     );
   }
