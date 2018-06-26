0c4f97db6e348985a7a251df29881cc3e8e34a51
Yann Jubelin <yann.jubelin@utopiamusic.com>
2018-06-26 17:37:01 +0200

    added ipfs to react

--- asset-ipfs-eth/src/App.js
+++ asset-ipfs-eth/src/App.js
@@ -1,11 +1,37 @@
 import React, { Component } from 'react';
+import ipfsAPI from 'ipfs-api'
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.ipfs = ipfsAPI('localhost', '5001')
+    this.state = {
+      id: null,
+      version: null,
+      protocol_version: null,
+      added_file_hash: null,
+      added_file_contents: null
+    }
+  }
+
+  componentDidMount() {
+    this.ipfs.id((err, res) => {
+      if (err) throw err
+      this.setState({
+        id: res.id,
+        version: res.agentVersion,
+        protocol_version: res.protocolVersion
+      })
+    })
+  }
+
   render() {
     return (
       <div className="App">
-
+        <p>Your ID is <strong>{this.state.id}</strong></p>
+        <p>Your IPFS version is <strong>{this.state.version}</strong></p>
+        <p>Your IPFS protocol version is <strong>{this.state.protocol_version}</strong></p>
       </div>
     );
   }
